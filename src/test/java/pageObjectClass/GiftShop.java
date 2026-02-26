15bb2d06aeae69de5fbecf3e448257567afee66a
Lakshmi Narayana <glnr8919@gmail.com>
2026-02-26 09:57:49 +0530

    test case twenty five

--- src/test/java/pageObjectClass/GiftShop.java
+++ src/test/java/pageObjectClass/GiftShop.java
@@ -14,22 +14,54 @@ public class GiftShop {
 		
 	}
 	
+	@FindBy(xpath="(//a[@class='site-nav__link site-nav__link--underline'])[2]")WebElement giftshoplink;
+	
+	
 	@FindBy(xpath="//a[@class='hero__slide-link']")WebElement getstarted;
 	
 	@FindBy(xpath="(//img[@loading='lazy'])[12]")WebElement moneyplantingiftshop;
 	
+	@FindBy(xpath="(//img[@loading='lazy'])[13]")WebElement calathearattlesnakeplantingiftshop;
 	
-	public void clickgetstarted() {
+	public void clickgetstarted() throws InterruptedException {
 		// click on the get started in gift shop
 		
+		
+		giftshoplink.click();
+		Thread.sleep(3000);
 		getstarted.click();
+		Thread.sleep(3000);
 	}
 	
 	
-	public void moneyplantingiftshop() {
+	public void moneyplantingiftshop() throws InterruptedException {
 		// click on the money tree plant
+		
+		giftshoplink.click();
+		Thread.sleep(3000);
 		moneyplantingiftshop.click();
+		Thread.sleep(3000);
+		
+		
 		
 		
 	}
+	
+	public void calathearattlesnakeplantingiftshop() throws InterruptedException {
+		
+		// calathea rattle snake plant in giftshop
+		giftshoplink.click();
+		Thread.sleep(3000);
+		
+		calathearattlesnakeplantingiftshop.click();
+		
+		
+		
+		Thread.sleep(3000);
+		
+	}
+	
+	
+	
+	
 }
